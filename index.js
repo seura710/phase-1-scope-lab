69c0dbe91caab8b4064c6903b0837ed6d56b1330
Harris Kim <seura710@gmail.com>
2021-10-13 23:18:19 -0400

    commit tree aa578ba48993f5c9a0a6c5e73dfec60f10023b54

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "chad"
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "lad"
+}
